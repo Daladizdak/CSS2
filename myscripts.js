c2c4e5868f7b28e5b3729c1418817e0f6aadd22e
Daladizdak <a.h.shekarinamini@wlv.ac.uk>
2025-04-08 01:18:01 +0100

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -54,17 +54,17 @@ tableRows += '<td>' + doc.data().movie_name + '</td>';
 tableRows += '<td>' + doc.data().movie_director + '</td>';
 tableRows += '<td>' + doc.data().movie_release + '</td>';
 tableRows += '<td>' + doc.data().movie_rating + '/5</td>';
-tableRows += '<td><button class="btn btn-danger btn-sm" id="deleteBtn" data-id="${doc.id}">Delete</button></td>';  
+tableRows += `<td><button class="btn btn-danger btn-sm deleteBtn" data-id="${doc.id}">Delete</button></td>`;  
 tableRows += '</tr>';
 });
 $('#reviewList').append(tableRows);
 
 //Delet button pressed
-$("#deleteBtn").click(function() {
-    const docId = this.data('id');
+$(".deleteBtn").click( async function() {
+    const docId = $(this).data('id');
     await deleteDoc(doc(db, "Movies", docId));
   
-}
+});
 
   
 // Add button pressed
